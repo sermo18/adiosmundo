1014f76eee1fef0d50e5ce4c48f2d705d515451a
Sergio Moreno <sermo14.sm@gmail.com>
2019-10-03 18:29:07 +0200

    experimentocolor

--- adiosmundo.java
+++ adiosmundo.java
@@ -7,9 +7,9 @@ public static void main (String[] args){
  
 	String amarillo= "\033[33m";	
 	String verde = "\033[32m";
-		
-	System.out.println(amarillo+"Adios Mundo");
-	System.out.println(verde+"Pero volvere");	
+	String reset = "\033[0m";		
+	System.out.println(amarillo+"Adios Mundo"+reset);
+	System.out.println(verde+"Pero volvere"+reset);	
 	
 		
 			
